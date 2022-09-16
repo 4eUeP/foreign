a8f677c4b8da384860dea43c5030b3a8753ce89c
mu <59917266+4eUeP@users.noreply.github.com>
2022-09-16 13:49:33 +0800

    add HsForeign.String (#1)

--- src/HsForeign.hs
+++ src/HsForeign.hs
@@ -1,7 +1,9 @@
 module HsForeign
   ( module HsForeign.Primitive
   , module HsForeign.AsyncFFI
+  , module HsForeign.String
   ) where
 
 import           HsForeign.AsyncFFI
 import           HsForeign.Primitive
+import           HsForeign.String
