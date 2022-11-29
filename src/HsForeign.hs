1484f7e0e8aa62427fd271863aa6e7d7ddf3494a
mu <59917266+4eUeP@users.noreply.github.com>
2022-11-29 16:27:25 +0800

    export HsForeign.CppStd in HsForeign module

--- src/HsForeign.hs
+++ src/HsForeign.hs
@@ -3,9 +3,11 @@ module HsForeign
   , module HsForeign.AsyncFFI
   , module HsForeign.String
   , module HsForeign.Utils
+  , module HsForeign.CppStd
   ) where
 
 import           HsForeign.AsyncFFI
+import           HsForeign.CppStd
 import           HsForeign.Primitive
 import           HsForeign.String
 import           HsForeign.Utils
